789f340a76fd04af8094bf2ab7e178c68a107413
Emerson MX <emersonmx@gmail.com>
2017-08-24 23:57:31 -0300

    Add base score entry script

---
+++ game/scripts/score_entry.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+onready var name_node = get_node('name')
+onready var score_node = get_node('score')
+
+func _ready():
+	pass
+
+func set_name(value):
+	name_node.set_text(value)
+
+func set_score(value):
+	score_node.set_text(value)
