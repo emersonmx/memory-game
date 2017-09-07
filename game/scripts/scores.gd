5c469f6c8c543ffe75152eb9e51f2004c0a51b6c
Emerson MX <emersonmx@gmail.com>
2017-09-07 01:40:52 -0300

    Show game after save score

--- game/scripts/scores.gd
+++ game/scripts/scores.gd
@@ -2,20 +2,16 @@ extends Node2D
 
 signal show_game
 
-var player_score = {}
+var player_score = {'name': '', 'miss': 0, 'time': 0}
+var wait_click = false
 
 onready var input_node = get_node('panel/input')
 onready var entries_node = get_node('score_entries')
+onready var timer_node = get_node('timer')
 
 func _ready():
 	input_node.connect('input_done', self, '_on_input_done')
-
-	set_process_input(true)
-	set_process(true)
-
-func _input(event):
-	if event.is_action_pressed("ui_select") and not event.is_echo():
-		emit_signal('show_game')
+	timer_node.connect('timeout', self, '_on_timeout')
 
 func _on_input_done(text):
 	if text.length() > 0:
@@ -23,3 +19,7 @@ func _on_input_done(text):
 		player_score['name'] = text
 		entries_node.save_score(player_score)
 		entries_node.show_entries()
+		timer_node.start()
+
+func _on_timeout():
+	emit_signal('show_game')
