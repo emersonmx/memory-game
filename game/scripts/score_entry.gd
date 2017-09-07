bf5ebd27c17d5bf58b42203c7603a7f377743f04
Emerson MX <emersonmx@gmail.com>
2017-09-07 02:41:51 -0300

    Add fades

--- game/scripts/score_entry.gd
+++ game/scripts/score_entry.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
+signal fadded_in
+signal fadded_out
+
 const MAX_SIZE = 20
 
 var name = ''
 var score = ''
 
 onready var text_node = get_node('text')
+onready var fade_in_timer_node = get_node('fade_in_timer')
+onready var fade_out_timer_node = get_node('fade_out_timer')
 
 func _ready():
-	pass
+	fade_in_timer_node.connect('timeout', self, '_on_fade_in_timeout')
+	fade_out_timer_node.connect('timeout', self, '_on_fade_out_timeout')
 
 func set_name(value):
 	name = value
@@ -18,8 +24,24 @@ func set_score(value):
 	score = value
 	_upload_text()
 
+func fade_out(time):
+	fade_out_timer_node.set_wait_time(time)
+	fade_out_timer_node.start()
+
+func fade_in(time):
+	fade_in_timer_node.set_wait_time(time)
+	fade_in_timer_node.start()
+
 func  _upload_text():
 	var dots = ''
 	for i in range(name.length() + score.length(), MAX_SIZE):
 		dots += '.'
 	text_node.set_text(name + dots + score)
+
+func _on_fade_in_timeout():
+	emit_signal('fadded_in')
+	show()
+
+func _on_fade_out_timeout():
+	emit_signal('fadded_out')
+	queue_free()
