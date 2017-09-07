022bfb1c9d582903db6448b91ccac064273bfb65
Emerson MX <emersonmx@gmail.com>
2017-09-07 03:29:34 -0300

    Fix bug when load score for the first time

--- game/scripts/score_entries.gd
+++ game/scripts/score_entries.gd
@@ -1,29 +1,63 @@
 extends Node2D
 
+signal entries_fadded_in
+signal entries_fadded_out
+
 const MAX_SCORES = 10
+const FADE_TIME = 0.1
+
+enum { FADE_NONE, FADE_OUT, FADE_IN }
+
+var fade_type = FADE_NONE
+var entries_count = 0
+var scores = []
 
 onready var entry_scene = preload('res://scenes/score_entry.tscn')
 
 func _ready():
-	show_entries()
+	_reset()
+
+func _reset():
+	fade_type = FADE_NONE
+	entries_count = 0
+	scores = _load_scores()
+
+func has_entries():
+	return scores.size() > 0
+
+func is_first_entry():
+	return scores.size() == 1
 
 func show_entries():
-	_clear_children()
+	if fade_type != FADE_NONE:
+		return
+
+	_reset()
+	fade_type = FADE_IN
 
 	var base_pos = Vector2()
 	var i = 0
-	for score in _load_scores():
+	for score in scores:
 		var entry = entry_scene.instance()
 		add_child(entry)
+		entry.connect('fadded_in', self, '_on_entry_fadded_in')
+		entry.connect('fadded_out', self, '_on_entry_fadded_out')
 		entry.set_pos(Vector2(base_pos.x, base_pos.y + (i * 10)))
 		entry.set_name(score['name'])
 		entry.set_score("%s/%s" % [str(score['miss']).pad_zeros(2),
 						str(score['time']).pad_zeros(3)])
+		entry.fade_in(FADE_TIME * (i + 1))
 		i += 1
 
-func _clear_children():
+func hide_entries():
+	if fade_type != FADE_NONE:
+		return
+
+	_reset()
+	fade_type = FADE_OUT
+
 	for i in range(0, get_child_count()):
-    	get_child(i).queue_free()
+    	get_child(i).fade_out(FADE_TIME * (i + 1))
 
 func _load_scores():
 	var scores = []
@@ -40,7 +74,8 @@ func _load_scores():
 	return scores
 
 func save_score(score):
-	var scores = _load_scores()
+	scores = _load_scores()
+
 	scores.append(score)
 	scores.sort_custom(self, '_score_sort')
 	if scores.size() > MAX_SCORES:
@@ -49,7 +84,7 @@ func save_score(score):
 	var f = File.new()
 	f.open('user://scores.csv', File.WRITE)
 	for s in scores:
-		f.store_string('%s,%s,%s\n' % [s['name'], s['miss'], s['time']])
+		f.store_string('"%s","%s","%s"\n' % [s['name'], s['miss'], s['time']])
 	f.close()
 
 func _score_sort(a, b):
@@ -59,4 +94,18 @@ func _score_sort(a, b):
 		if a['time'] < b['time']:
 			return true
 
-	return false
+	return
+
+func _fade_emit_signal(fade_signal, fade_type):
+	if self.fade_type != fade_type:
+		return
+	entries_count += 1
+	if entries_count >= scores.size()-1:
+		_reset()
+		emit_signal(fade_signal)
+
+func _on_entry_fadded_in():
+	_fade_emit_signal('entries_fadded_in', FADE_IN)
+
+func _on_entry_fadded_out():
+	_fade_emit_signal('entries_fadded_out', FADE_OUT)
--- game/scripts/scores.gd
+++ game/scripts/scores.gd
@@ -3,7 +3,7 @@ extends Node2D
 signal show_game
 
 var player_score = {'name': '', 'miss': 0, 'time': 0}
-var wait_click = false
+var fade_in_count = 0
 
 onready var input_node = get_node('panel/input')
 onready var entries_node = get_node('score_entries')
@@ -12,14 +12,32 @@ onready var timer_node = get_node('timer')
 func _ready():
 	input_node.connect('input_done', self, '_on_input_done')
 	timer_node.connect('timeout', self, '_on_timeout')
+	entries_node.connect('entries_fadded_in', self, '_on_entries_fadded_in')
+	entries_node.connect('entries_fadded_out', self, '_on_entries_fadded_out')
+
+	if not entries_node.has_entries():
+		return
+
+	entries_node.show_entries()
 
 func _on_input_done(text):
 	if text.length() > 0:
 		get_node('panel').hide()
 		player_score['name'] = text
 		entries_node.save_score(player_score)
-		entries_node.show_entries()
-		timer_node.start()
+		if entries_node.is_first_entry():
+			entries_node.show_entries()
+		else:
+			entries_node.hide_entries()
 
 func _on_timeout():
 	emit_signal('show_game')
+
+func _on_entries_fadded_in():
+	fade_in_count += 1
+	if fade_in_count <= 1:
+		return
+	timer_node.start()
+
+func _on_entries_fadded_out():
+	entries_node.show_entries()
